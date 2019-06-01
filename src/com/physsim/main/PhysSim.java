a8e77cb433a27d38a2924712c3271c4ede3acce6
Mirza Mehmedagic <mmehmedagic@cps.edu>
2019-06-01 15:10:56 -0500

    Third commit

--- src/com/physsim/main/PhysSim.java
+++ src/com/physsim/main/PhysSim.java
@@ -2,10 +2,10 @@ package com.physsim.main;
 
 public class PhysSim {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Hello world");
 
-        //It's a comment
+        // It's a comment
     }
 
 }
