8d98414638068bd6d91e8d3baac94d0a3429c493
Mirza Mehmedagic <mmehmedagic@cps.edu>
2019-06-01 17:26:08 -0500

    Add simple intersections and publicized fields

--- src/com/physsim/main/AABB.java
+++ src/com/physsim/main/AABB.java
@@ -8,12 +8,14 @@ public class AABB extends PhysicsObject {
     public Point2D.Float min; //Upper left
     public Point2D.Float max; //Lower right
 
-    public AABB(Point2D.Float min, Point2D.Float max) {
+    public AABB(float mass, Point2D.Float min, Point2D.Float max) {
+        super(mass);
         this.min = min;
         this.max = max;
     }
 
-    public AABB(float minX, float minY, float maxX, float maxY) {
+    public AABB(float mass, float minX, float minY, float maxX, float maxY) {
+        super(mass);
         this.min = new Point2D.Float(minX, minY);
         this.max = new Point2D.Float(maxX, maxY);
     }
