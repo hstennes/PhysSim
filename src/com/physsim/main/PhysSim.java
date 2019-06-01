16b829b84eb9991c80143ac5f77db72b2f8c53ad
Hank Stennes <hpstennes@gmail.com>
2019-06-01 15:29:24 -0500

    Add game loop again

--- src/com/physsim/main/PhysSim.java
+++ src/com/physsim/main/PhysSim.java
@@ -1,11 +1,89 @@
 package com.physsim.main;
 
-public class PhysSim {
+import java.awt.Canvas;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Toolkit;
+import java.awt.image.BufferStrategy;
 
-    public static void main(String[] args){
-        System.out.println("Hello world");
+public class PhysSim extends Canvas implements Runnable{
 
-        //It's a comment
+    private static final long serialVersionUID = 1L;
+    private static Dimension resolution = Toolkit.getDefaultToolkit().getScreenSize();
+    public static final int WIDTH = (int) resolution.getWidth(), HEIGHT = (int) resolution.getHeight() - 100;
+    private boolean running = false;
+
+    private Thread thread;
+
+    public PhysSim(){
+        new Window(WIDTH, HEIGHT, "TANKS!", this);
+        System.out.println("Application started");
     }
 
+    public synchronized void start(){
+        thread = new Thread(this);
+        thread.start();
+        running = true;
+    }
+
+    public synchronized void stop(){
+        try{
+            thread.join();
+            running = false;
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        long lastTime = System.nanoTime();
+        double amountOfTicks = 60.0;
+        double ns = 1000000000 / amountOfTicks;
+        double delta = 0;
+        long timer = System.currentTimeMillis();
+        int updates = 0;
+        int frames = 0;
+        while(running) {
+            long now = System.nanoTime();
+            delta += (now - lastTime) / ns;
+            lastTime = now;
+            while(delta >= 1){
+                tick();
+                updates++;
+                delta--;
+            }
+            render();
+            frames++;
+            if(System.currentTimeMillis() - timer > 1000){
+                timer += 1000;
+                System.out.println("FPS: " + frames + " TICKS: " + updates);
+                frames = 0;
+                updates = 0;
+            }
+        }
+        stop();
+    }
+
+    private void tick(){
+
+    }
+
+    private void render(){
+        BufferStrategy bs = this.getBufferStrategy();
+        if(bs == null){
+            this.createBufferStrategy(2);
+            return;
+        }
+        Graphics g = bs.getDrawGraphics();
+        Graphics2D g2d = (Graphics2D) g;
+
+        g.dispose();
+        bs.show();
+    }
+
+    public static void main(String[] x){
+        new PhysSim();
+    }
 }
--- src/com/physsim/main/PhysicsObject.java
+++ src/com/physsim/main/PhysicsObject.java
@@ -2,6 +2,4 @@ package com.physsim.main;
 
 public class PhysicsObject {
 
-
-
 }
