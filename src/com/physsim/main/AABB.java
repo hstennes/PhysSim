21af0743d486a56987b68959a89b01554464f467
Mirza Mehmedagic <mmehmedagic@cps.edu>
2019-06-01 16:28:20 -0500

    Finish AABB class

--- src/com/physsim/main/AABB.java
+++ src/com/physsim/main/AABB.java
@@ -5,23 +5,29 @@ import java.awt.geom.Point2D;
 
 public class AABB extends PhysicsObject {
 
-    private Point2D.Float min;
-    private Point2D.Float max;
+    private Point2D.Float min; //Upper left
+    private Point2D.Float max; //Lower right
 
     public AABB(Point2D.Float min, Point2D.Float max) {
         this.min = min;
         this.max = max;
     }
 
+    public AABB(float minX, float minY, float maxX, float maxY) {
+        this.min = new Point2D.Float(minX, minY);
+        this.max = new Point2D.Float(maxX, maxY);
+    }
+
+
     public boolean intersects(PhysicsObject object) {
         if (object instanceof AABB) {
             AABB aabb = (AABB) object;
-            // Exit with no intersection if found separated along an axis
+            //Exit with no intersection if found separated along an axis
             if (aabb.max.x < min.x || aabb.min.x > max.x) return false;
             if (aabb.max.y < min.y || aabb.min.y > max.y) return false;
         }
 
-        // No separating axis found, therefore there is at least one overlapping axis
+        //No separating axis found, therefore there is at least one overlapping axis
         return true;
     }
 
--- src/com/physsim/main/PhysicsObject.java
+++ src/com/physsim/main/PhysicsObject.java
@@ -3,5 +3,4 @@ package com.physsim.main;
 public abstract class PhysicsObject {
 
     public abstract void tick();
-
 }
